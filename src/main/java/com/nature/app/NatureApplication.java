41206206f479e4cde46959c591fa1b1d8352a679
Akhila-Gardas <akhilagm28@gmail.com>
2026-09-16 19:48:21 +0530

    Update NatureApplication.java

---
+++ src/main/java/com/nature/app/NatureApplication.java
@@ -0,0 +1,12 @@
+package com.nature.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatureApplication.class, args);
+    }
+}
